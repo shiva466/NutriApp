1ab8ff7a58c8cc0920d036ff5cc45527b4cbcc9e
shiva466 <37341802+shiva466@users.noreply.github.com>
2023-04-24 03:39:16 +0530

    Update NutritionControllerTest.java

--- src/test/java/ait/student/nutrition/NutritionControllerTest.java
+++ src/test/java/ait/student/nutrition/NutritionControllerTest.java
@@ -2,8 +2,6 @@ package ait.student.nutrition;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -12,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -29,6 +28,87 @@ import ait.student.nutrition.rep.NutritionRepository;
 
 class NutritionControllerTest {
 
+
+	  private supplier supplier;
+
+	    @BeforeEach
+	    public void setUp() {
+	        supplier = new supplier(1L, "Shiva Manufactures", "Books", "Shiva1", "10.00", "20.00", "5.00");
+	    }
+
+	    @Test
+	    public void testGetId() {
+	        assertThat(supplier.getId().longValue()).isEqualTo(1L);
+	    }
+
+	    @Test
+	    public void testGetSupplier_name() {
+	        assertThat(supplier.getSupplier_name()).isEqualTo("Shiva Manufactures");
+	    }
+
+	    @Test
+	    public void testGetItem() {
+	        assertThat(supplier.getItem()).isEqualTo("Books");
+	    }
+
+	    @Test
+	    public void testGetItem_code() 
+	    {
+	        assertThat(supplier.getItem_code()).isEqualTo("Shiva1");
+	    }
+
+	    @Test
+	    public void testGetManufacting_cost() 
+	    {
+	        assertThat(supplier.getManufacting_cost()).isEqualTo("10.00");
+	    }
+
+	    @Test
+	    public void testGetSelling_cost() 
+	    {
+	        assertThat(supplier.getSelling_cost()).isEqualTo("20.00");
+	    }
+
+	    @Test
+	    public void testGetProfit_gained() {
+	        assertThat(supplier.getProfit_gained()).isEqualTo("5.00");
+	    }
+
+	    @Test
+	    public void testSetSupplier_name() {
+	        supplier.setSupplier_name("Paul");
+	        assertThat(supplier.getSupplier_name()).isEqualTo("Paul");
+	    }
+
+	    @Test
+	    public void testSetItem() {
+	        supplier.setItem("Pens");
+	        assertThat(supplier.getItem()).isEqualTo("Pens");
+	    }
+
+	    @Test
+	    public void testSetItem_code() {
+	        supplier.setItem_code("Pen1");
+	        assertThat(supplier.getItem_code()).isEqualTo("Pen1");
+	    }
+
+	    @Test
+	    public void testSetManufacting_cost() {
+	        supplier.setManufacting_cost("15.00");
+	        assertThat(supplier.getManufacting_cost()).isEqualTo("15.00");
+	    }
+
+	    @Test
+	    public void testSetSelling_cost() {
+	        supplier.setSelling_cost("25.00");
+	        assertThat(supplier.getSelling_cost()).isEqualTo("25.00");
+	    }
+
+	    @Test
+	    public void testSetProfit_gained() {
+	        supplier.setProfit_gained("10.00");
+	        assertThat(supplier.getProfit_gained()).isEqualTo("10.00");
+	    }
     @Mock
     private NutritionRepository nutritionRepository;
 
@@ -43,9 +123,10 @@ class NutritionControllerTest {
         mockNutri = MockMvcBuilders.standaloneSetup(nutritionController).build();
     }
 
+    
     @Test
     void testGetSuppliers() throws Exception {
-        List<supplier> suppliers = new ArrayList<supplier>();
+    	List<supplier> suppliers = new ArrayList<supplier>();
         suppliers.add(new supplier(1L,"Supplier 1", "Item 1", "IC1", "10.0", "15.0", "5.0"));
         suppliers.add(new supplier(2L,"Supplier 2", "Item 2", "IC2", "20.0", "25.0", "5.0"));
         suppliers.add(new supplier(3L,"Supplier 3", "Item 3", "IC3", "30.0", "35.0", "5.0"));
@@ -71,6 +152,7 @@ class NutritionControllerTest {
                 .andExpect(jsonPath("$[2].manufacting_cost").value(30.0))
                 .andExpect(jsonPath("$[2].selling_cost").value(35.0))
                 .andExpect(jsonPath("$[2].profit_gained").value(5.0));
+        
     }
     @Test
     void testGetApplication() {
